c5e13a43e4ab3872716ad78fcb56dfc6e7a86d50
AntonShch <info.oth@yandex.ru>
2019-10-26 16:44:11 +0500

    Страницы акции подробная + услуг + доделал все услуги

---
+++ src/blocks/components/services/services.js
@@ -0,0 +1,26 @@
+import Swiper from 'swiper';
+new Swiper('.services-special-offers__slider', {
+    speed: 400,
+    slidesPerView: 2,
+		navigation: {
+		nextEl: '.services-special-offers .slider-arrows__right',
+		prevEl: '.services-special-offers .slider-arrows__left',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 24
+      },
+      // when window width is >= 640px
+      568: {
+        slidesPerView: 2,
+        spaceBetween: 24
+      },
+      // when window width is >= 640px
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 24
+      },
+    }
+});
--- src/js/import/components.js
+++ src/js/import/components.js
@@ -2,4 +2,5 @@ import "%components%/sliderHome/sliderHome";
 import "%components%/ourTeam/ourTeam";
 import "%components%/fastLinks/fastLinks";
 import "%components%/addressMap/addressMap";
+import "%components%/services/services";
 // import "%components%/map/map";
